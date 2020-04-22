7be772ad0a69897339e04f60787c08b4a18119f4
Nick Debeurre <ndebeurr@asu.edu>
2020-04-21 19:29:26 -0700

    problems 1-3

---
+++ 2.py
@@ -0,0 +1,13 @@
+def problem2():
+    sum = 0
+    first = 1
+    second = 2
+    while second < 4000000:
+        if second % 2 == 0:
+            sum += second
+        temp = second
+        second += first
+        first = temp
+    return sum
+
+print(problem2())
