bd1dd4e347d896591e550c7049c33dc2da4c6254
Nick Debeurre <ndebeurr@asu.edu>
2020-05-06 21:19:35 -0700

    problem 5 complete

---
+++ 5.py
@@ -0,0 +1,27 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def divBy(num, divBy):
+    if num % divBy == 0:
+        return True
+    else:
+        return False
+
+def problem5_bruteForce():
+    smallest_mult = 1
+    while 1:
+        if divBy(smallest_mult, 20) and divBy(smallest_mult, 19) and \
+            divBy(smallest_mult, 19) and divBy(smallest_mult, 18) and \
+            divBy(smallest_mult, 17) and divBy(smallest_mult, 16) and \
+            divBy(smallest_mult, 15) and divBy(smallest_mult, 14) and \
+            divBy(smallest_mult, 13) and divBy(smallest_mult, 12) and \
+            divBy(smallest_mult, 11) and divBy(smallest_mult, 10) and \
+            divBy(smallest_mult, 9) and divBy(smallest_mult, 8) and \
+            divBy(smallest_mult, 7) and divBy(smallest_mult, 6) and \
+            divBy(smallest_mult, 5) and divBy(smallest_mult, 4) and \
+            divBy(smallest_mult, 3) and divBy(smallest_mult, 2):
+            return smallest_mult
+        smallest_mult += 1
+
+if __name__ == '__main__':
+    print('The smallest multiple is:', problem5_bruteForce())
