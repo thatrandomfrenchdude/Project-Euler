ae154ebe87b3d050e2056b6473cb7ce52aa0452e
Nicholas Debeurre <debeurren@gmail.com>
2024-01-20 11:26:49 -0800

    generalize problem 1

---
+++ 1-10/1.py
@@ -0,0 +1,19 @@
+# sum of multiples of a and b below limit
+# generalized solution for problem 1
+def sum_of_multiples(a, b, limit):
+    sum = 0
+    for i in range(limit):
+        if i % a == 0 or i % b == 0:
+            sum += i
+    return sum
+
+# example: sum of multiples of 3 and 5 below 1000
+def problem1():
+    sum = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
+
+if __name__ == '__main__':
+    print(problem1())
